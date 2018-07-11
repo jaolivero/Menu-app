6042aacf58fca4ac3bba1108775fcf12b965b26e
Joel Olivero <joelolivero@outlook.com>
2018-07-11 18:18:53 -0400

    Update lotsofmenu.py

--- lotsofmenu.py
+++ lotsofmenu.py
@@ -4,28 +4,21 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem, User
 
 engine = create_engine('sqlite:///restaurantmenuwithusers.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
+
 session = DBSession()
 
 
-# Create dummy user
+
 User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
              picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
 session.add(User1)
 session.commit()
 
-# Menu for UrbanBurger
+
 restaurant1 = Restaurant(user_id=1, name="Urban Burger")
 
 session.add(restaurant1)
@@ -213,7 +206,7 @@ session.add(menuItem2)
 session.commit()
 
 
-# Menu for Tony's Bistro
+
 restaurant1 = Restaurant(user_id=1, name="Tony\'s Bistro ")
 
 session.add(restaurant1)
@@ -251,7 +244,7 @@ session.add(menuItem5)
 session.commit()
 
 
-# Menu for Andala's
+
 restaurant1 = Restaurant(user_id=1, name="Andala\'s")
 
 session.add(restaurant1)
